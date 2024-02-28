a71bd59681623ef60770a615c35e4fe91d307c3e
Edgar Dimas Isaadrazak <edgar.dimas.ir@gmail.com>
2024-02-28 08:23:16 +0700

    controller feat.

---
+++ app/controllers/About.php
@@ -0,0 +1,9 @@
+<?php
+class About {
+  public function index( $nama = 'Edgar', $pekerjaan = 'Programmer'){
+    echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan";
+  }
+  public function page(){
+    echo "About/page";
+  }
+}
